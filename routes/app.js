3d0f9b4947e8e01b347f818658021b799586f104
Srivatsa S <srivatsashankaran@gmail.com>
2017-11-17 01:41:51 +0530

    Fetching Data from the Database

--- routes/app.js
+++ routes/app.js
@@ -2,17 +2,22 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
-router.get('/', function (req, res, next) {
+/* router.get('/', function (req, res, next) {
     res.render('node');
-});
+}); */
 
 /* router.get('/message/:msg', function (req, res, next) {
     res.render('node', {message: req.params.msg});
 });
  */
 
-router.get('/message', function (req, res, next) {
-    res.render('node');
+router.get('/', function (req, res, next) {
+    User.findOne({}, function(err, doc) {
+        if(err) {
+            return res.send('Error!');
+        }
+        res.render('node', {email: doc.email});
+    });
 });
 
 
