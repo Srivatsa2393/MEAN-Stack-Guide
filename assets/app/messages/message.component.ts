c95bd60ce0abac6aa79bd25c74ff53a23325a511
Srivatsa S <srivatsashankaran@gmail.com>
2017-11-17 22:05:33 +0530

    Using property binding

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Message } from './message.model';
 
 @Component({
     selector: 'app-message',
@@ -21,5 +22,5 @@ import { Component } from '@angular/core';
 })
 
 export class MessageComponent {
-
+    @Input() message: Message;
 }
