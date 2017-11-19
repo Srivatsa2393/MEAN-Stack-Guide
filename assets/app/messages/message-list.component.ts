c8c6594be48264b0a7e83fd7bfeaeb3dd5bf2421
Srivatsa S <srivatsashankaran@gmail.com>
2017-11-19 20:22:58 +0530

    One instance to rule them all

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -13,8 +13,7 @@ import { MessageService } from './message.service';
                 *ngFor = "let message of messages"
             ></app-message>
         </div>
-    `,
-    providers: [MessageService]
+    `
 })
 
 export class MessageListComponent implements OnInit{
