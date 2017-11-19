277d3d54c7761913915ef2db692e4cb9e1e58a12
Srivatsa S <srivatsashankaran@gmail.com>
2017-11-19 20:09:39 +0530

    Adding messages with a service

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 
+import { MessageService } from './message.service';
+import { Message } from './message.model';
+
 @Component({
     selector: 'app-message-input',
-    templateUrl: './message-input.component.html'
+    templateUrl: './message-input.component.html',
+    providers: [MessageService]
 })
 
 export class MessageInputComponent{
+
+    constructor(private messageService: MessageService) {}
     onSave(value: string) {
-        console.log(value);
+        //console.log(value);
+        const message = new Message(value, 'Srivatsa');
+        this.messageService.addMessage(message);
     }
 }
