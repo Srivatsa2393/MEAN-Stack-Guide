299898e6af13eec7f56ff5a47f2406278329c64f
Srivatsa S <srivatsashankaran@gmail.com>
2017-11-21 18:15:41 +0530

    Finishing the frontend by hooking the delete message

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Message } from './message.model';
+import { MessageService } from './message.service';
 
 @Component({
     selector: 'app-message',
@@ -25,10 +26,16 @@ export class MessageComponent {
     @Input() message: Message;
     @Output() editClicked = new EventEmitter<string>(); 
 
+    constructor(private messageService: MessageService) {}
+
     color = 'red';
 
     onEdit() {
         alert('It worked');
         this.editClicked.emit('A new value');
+    }
+
+    onDelete() {
+        this.messageService.deleteMessage(this.message);
     }
 }
